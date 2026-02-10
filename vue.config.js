c36e17af54f1379d872a4405bbf0ec167b91156c
lzy <305443291@qq.com>
2026-02-10 19:48:27 +0800

    引入elementui

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 9527,
+    open: true,
+    // 阻止浏览器弹出错误
+    client: {
+      overlay: false
+    }
+  },
 })
