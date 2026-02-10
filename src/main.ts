d54eb4578ae6c4d8dafaac92d548993c94d02e90
lzy <305443291@qq.com>
2026-02-10 20:24:23 +0800

    测试element

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入ElementUI
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(ElementUI);
+
+import "@/scss/main.scss"
+
 Vue.config.productionTip = false
 
 new Vue({
